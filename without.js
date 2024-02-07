eefa421185ebc8c7b258c4e3868205fe18836fcc
Daniel Pho <daniel.phou@hotmail.com>
2024-02-06 23:57:01 -0500

    added without function that includes 'assertEqual' and 'assertArraysEqual' functions

---
+++ without.js
@@ -0,0 +1,33 @@
+// iterates through array and compares
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+// assertion messages here
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`🟢🟢🟢Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+// function to remove items
+const without = function(source, itemsToRemove) {
+  let altered = [];
+for (let i = 0; i < source.length; i++) {
+  if (source[i] !== itemsToRemove) {
+    altered.push(source[i]);
+  }
+}
+return altered;
+};
