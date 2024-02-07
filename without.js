ecf8e44ade11b5eae631ca89920d8beaa18b1eaa
Daniel Pho <daniel.phou@hotmail.com>
2024-02-07 01:02:02 -0500

    attempted another fix for without function

--- without.js
+++ without.js
@@ -26,12 +26,12 @@ const without = function(source, itemsToRemove) {
   let altered = [];
   for (let i = 0; i < source.length; i++) {
     for (let j = 0; j < itemsToRemove.length; j++) {
-      if (source[i] !== itemsToRemove[i]) {
+      if (source[i] !== itemsToRemove[j]) {
         altered.push(source[i]);
       }
     }
-    return altered;
   }
+  return altered;
 };
 
-console.log(assertArraysEqual(without([1, 2, 3], [1])));
+assertArraysEqual(without([1, 2, 3], [1]));
