00915835eb2cfa3aff90c0a4950c750426c8195c
Daniel Pho <daniel.phou@hotmail.com>
2024-02-07 17:20:14 -0500

    added code to evaluate cases less than 2 elements

--- middle.js
+++ middle.js
@@ -22,14 +22,14 @@ const assertArraysEqual = function(arr1, arr2) {
 };
 
 const middle = function(array) {
-//array must have numbers in it, if not return response
-  if (array.length === 0) {
-    return "Please provide valid response";
-  }
   // during initial run through code got hard to follow use this variable instead for expressions
   const mid = array.length / 2;
+  //array must have numbers in it, if not return response
+  if (array.length <= 2) {
+    return [];
+  }
   // if array is odd i can take length and divide by 2 round down and return the middle element
-  if (array.length % 2 === 1) {
+   else if (array.length % 2 === 1) {
     return array[Math.floor(mid)];
   }
   // if array is even divide length by 2 minus 1 (starting point) and then plus 1.
@@ -37,6 +37,9 @@ const middle = function(array) {
     return [array[mid - 1], array[mid]];
   }
 };
-assertArraysEqual(middle([]), "Please provide valid response");
+assertArraysEqual(middle([1]), []);
 assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]);
 assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]);
+
+// cases with 1 or 2 elements
+// cases with less than 3
