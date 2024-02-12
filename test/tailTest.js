d44fbf0410cee03f082b5da4c08baae843c6188c
Daniel Pho <daniel.phou@hotmail.com>
2024-02-12 17:19:18 -0500

    refactored using chai assertion library

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,16 @@
 const tail = require("../tail");
 const assertEqual = require("../assertEqual");
+const { assert } = require("chai");
 
 const array = tail([1, 2, 3, 4, 5]);
 const onlyOne = tail([1]);
 
-assertEqual(array.length, 4);
-assertEqual(onlyOne.length, 0);
-assertEqual(array[0], 2);
+// assertEqual(array.length, 4);
+// assertEqual(onlyOne.length, 0);
+// assertEqual(array[0], 2);
+
+describe("#tail", () => {
+  it("should return 4 when given [1, 2, 3, 4, 5]", () => {
+    assert.equal((array.length), 4);
+  })
+});
