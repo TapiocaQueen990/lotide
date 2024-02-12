f3427ef7ee17082de6c11987e7033ffa1b0d11c2
Daniel Pho <daniel.phou@hotmail.com>
2024-02-12 16:29:04 -0500

    changed testing to accomodate chai and mocha

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,15 @@
 
+const { assert } = require("chai");
 const assertEqual = require("../assertEqual");
 const head = require("../head");
 
-assertEqual(head([5,6,7]), 5);
+// assertEqual(head([5,6,7]), 5);
+
+describe("#head", () => {
+it("returns 1 for [1, 2, 3]", () => {
+  assert.strictEqual(head([1, 2, 3]), 1);
+});
+it("should return '5' for [5]", () => {
+  assert.strictEqual(head([5]), 5);
+})
+});
