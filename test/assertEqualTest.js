5a9810d699ca00a26d0b8867367ee7cffbc1e137
Daniel Pho <daniel.phou@hotmail.com>
2024-02-12 15:13:56 -0500

    created test folder to seperate code from test function

---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require("../assertEqual");
+
+// Test code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("cookie", "cookie");
+assertEqual(12, 3);
